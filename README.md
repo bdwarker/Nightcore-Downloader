c0fef57cd7bd70cab7d2a248c785858dcf0e6efa
Unerasable <shayanariz99@gmail.com>
2022-03-09 22:33:16 +0530

    Update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Nightcore-Downloader
 
 ## How to use
-Clone the project and go the the project directory, open command prompt in that directory, type `pip install -r requirements.txt`. After that type `py main.py` if you're on windows, if you're on mac/linux type `python main.py`.
+Clone the project and go the the project directory, open command prompt in that directory, type `pip install -r requirements.txt`. After that type `py main.py` if you're on windows, if you're on mac/linux type `python main.py`. (Don't worry if the app shows "Not responding" it is just downloading the music)
+
+## Features
+• Automatically makes the files so that they will be able to show in Discord's Rich Presence in spotify.
+• Able to download whole playlists.
+• Simple to use.
--- main.py
+++ main.py
@@ -7,9 +7,26 @@ import time
 import requests
 import colorama
 from colorama import Fore, Style
+import tkinter as tk
+from plyer import notification
+import webbrowser
+
+app_Version = "1.1.0"
+application_get_url = "https://unerasable.github.io/application.json"
+def check_valid_status_code(request):
+    if request.status_code == 200:
+        return request.json()
+
+    return False
 
 
-def download():
+def get_application_get_url():
+    request = requests.get(application_get_url)
+    data = check_valid_status_code(request)
+    return data
+app_URL=get_application_get_url()
+checkAppVer = f"{app_URL['nightcoreDownloader']['verson']}"
+def download(playlist, album_input, album_artist_input, save_Loc):
     for video in playlist.videos:
         try:
             print(f'Downloading {video.title}...')
@@ -30,56 +47,74 @@ def download():
             song.tag.save()
             os.remove("%s.jpg" % os.path.join(save_Loc, video.title))
             os.system(f'eyed3 "{os.path.join(save_Loc, video.title)}.mp3"')
+            notification.notify(title= 'Downloaded',
+                    message= f'{video.title} has been downloaded',
+                    app_icon = 'app.ico',
+                    timeout= 0.1,
+                    toast=False)
         except:
             print(f'{Fore.RED}[ERROR]{Fore.RESET} {video.title} failed to download')
             print(f"{Fore.YELLOW}[WARNING]{Fore.RESET} Using the video ID to download...")
-            video = str(video.video_id)
-            video.streams.first().download(output_path=save_Loc, filename=video+ ".mp4")
+            video.streams.first().download(output_path=save_Loc, filename=str(video.video_id)+ ".mp4")
             response = requests.get(str(video.thumbnail_url))
-            with open(os.path.join(save_Loc, f"{video}.jpg"), "wb") as f:
+            with open(os.path.join(save_Loc, f"{str(video.video_id)}.jpg"), "wb") as f:
                 f.write(response.content)
-            videoe = VideoFileClip("%s.mp4" % os.path.join(save_Loc, video))
-            videoe.audio.write_audiofile("%s.mp3" % os.path.join(save_Loc, video))
+            videoe = VideoFileClip("%s.mp4" % os.path.join(save_Loc, str(video.video_id)))
+            videoe.audio.write_audiofile("%s.mp3" % os.path.join(save_Loc, str(video.video_id)))
             videoe.close()
-            os.remove("%s.mp4" % os.path.join(save_Loc, video))
-            song = eyed3.load(f"{os.path.join(save_Loc, video)}.mp3")
-            song.tag.images.set(3, open(f"{os.path.join(save_Loc, f'{video}.jpg')}", 'rb').read(), 'image/jpeg')
+            os.remove("%s.mp4" % os.path.join(save_Loc, str(video.video_id)))
+            song = eyed3.load(f"{os.path.join(save_Loc, str(video.video_id))}.mp3")
+            song.tag.images.set(3, open(f"{os.path.join(save_Loc, f'{str(video.video_id)}.jpg')}", 'rb').read(), 'image/jpeg')
             song.tag.artist = f"{video.author}"
             song.tag.album = f"{album_input}"
             song.tag.album_artist = f"{album_artist_input}"
             song.tag.title = f"{video.title}"
             song.tag.save()
-            os.remove("%s.jpg" % os.path.join(save_Loc, video))
-            os.system(f'eyed3 "{os.path.join(save_Loc, video)}.mp3"')
-
-
-
-
-
-
-def take_PL():
-    global playlist
-    global save_Loc
-    global album_input
-    global album_artist_input
-    pl = input("Enter playlist link: ")
-    if pl == "":
+            os.remove("%s.jpg" % os.path.join(save_Loc, str(video.video_id)))
+            os.system(f'eyed3 "{os.path.join(save_Loc, str(video.video_id))}.mp3"')
+            notification.notify(title= 'Downloaded',
+                    message= f'{video.title} has been downloaded',
+                    app_icon = 'app.ico',
+                    timeout= 0.1,
+                    toast=False)
+def take_PL(pl, album_input, album_artist_input, save_Loc):
+    if pl.get() == "":
         print("No playlist link entered")
         take_PL()
-    playlist = Playlist(f'{pl}')
+    playlist = Playlist(f'{pl.get()}')
     print(f'Name of the playlist: {playlist.title}, Number of videos in playlist: {len(playlist.video_urls)}')
-    album_input = input("Album name: ")
-    if album_input == "":
+    if album_input.get() == "":
         album_input = "Unknown"
-    album_artist_input = input("Album artist name: ")
-    if album_artist_input == "":
+    if album_artist_input.get() == "":
         album_artist_input = "Unknown"
-    save_Loc = input("Save location: ")
-    if save_Loc == "":
+    if save_Loc.get() == "":
         if os.path.exists("music"):
             save_Loc = "music"
         else:
             os.mkdir("music")
             save_Loc = "music"
-    download()
-take_PL()
+    download(playlist, album_input.get(), album_artist_input.get(), save_Loc.get())
+
+def makeWindow():
+    global root
+    root = tk.Tk()
+    root.title("Nightcore Downloader")
+    root.geometry("300x200")
+    root.resizable(False, False)
+    root.configure(background='#24283b')
+    tk.Label(root, text="Nightcore Downloader", font=("Helvetica", 16), fg="white", bg="#24283b").pack()
+    if checkAppVer != app_Version:
+        tk.Label(root, text="New Update Available", font=("Helvetica", 12), fg="white", bg="#24283b").pack()
+        tk.Button(root, text="Update", command=lambda: webbrowser.open("https://www.github.com/unerasable/nightcore-downloader/releases")).pack()
+    else:
+        pl = tk.Entry(root)
+        pl.pack()
+        album_input = tk.Entry(root)
+        album_input.pack()
+        album_artist_input = tk.Entry()
+        album_artist_input.pack()
+        save_Loc = tk.Entry(root)
+        save_Loc.pack()
+        tk.Button(root, text="Download", command=lambda: take_PL(pl,album_input,album_artist_input,save_Loc)).pack()
+    root.mainloop()
+makeWindow()
